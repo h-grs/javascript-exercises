2c78f36c81ee6f3fb72b2891929c14311ad88078
hgrs <hugo.groschaus@protonmail.com>
2026-02-26 15:39:40 +0100

    Add Exercice 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr) {
+  const toDelete = Array.prototype.slice.call(arguments, 1);
+
+  return arr.filter((elem) => !toDelete.includes(elem));
 };
 
 // Do not edit below this line
