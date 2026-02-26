40fc20fcffdf357ad64b223cba35a21fa9b3248b
hgrs <hugo.groschaus@protonmail.com>
2026-02-26 16:07:40 +0100

    Add Exercice 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (F) {
+  var rounder = Math.pow(10, 1);
+
+  return Math.round((5 / 9) * (F - 32) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (C) {
+  return Math.round(((C * 9) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
