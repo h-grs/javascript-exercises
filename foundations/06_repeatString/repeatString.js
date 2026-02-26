81b13d22112bd93d412a7b106dc3e0cca61e6ad8
hgrs <hugo.groschaus@protonmail.com>
2026-02-26 15:28:47 +0100

    Add Exercice 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (str, x) {
+  let finalString = "";
 
+  if (x < 0) {
+    return "ERROR";
+  }
+
+  for (let i = 0; i < x; i++) {
+    finalString += str;
+  }
+  return finalString;
 };
 
 // Do not edit below this line
