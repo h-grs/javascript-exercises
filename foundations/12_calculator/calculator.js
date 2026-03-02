314f3364257f20b5f65535c66c13e61900d04521
hgrs <hugo.groschaus@protonmail.com>
2026-03-02 15:54:25 +0100

    refactor(Factorial): function recursive now

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -22,14 +22,7 @@ const power = function (a, b) {
 };
 
 const factorial = function (n) {
-  if (n === 0 || n === 1) { return 1; }
-
-  let res = 1;
-
-  for (let i = 1; i <= n; i++) {
-    res *= i;
-  }
-  return res;
+  if (n === 0 || n === 1) { return 1; } else { return n * factorial(n - 1); }
 };
 
 // Do not edit below this line
