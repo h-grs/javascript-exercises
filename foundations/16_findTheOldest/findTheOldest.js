465ff038e8cbdbea37667a20c28b1652a0bacb94
hgrs <hugo.groschaus@protonmail.com>
2026-03-03 09:44:05 +0100

    Completed ex 16 - findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function (peoples) {
+    return peoples.reduce((accumulator, next) => {
+        if (!accumulator.yearOfDeath) { accumulator.yearOfDeath = 2026 }
+        if (!next.yearOfDeath) { next.yearOfDeath = 2026 }
 
+        let accumulatorAge = accumulator.yearOfDeath - accumulator.yearOfBirth
+        let nextAge = next.yearOfDeath - next.yearOfBirth
+
+        return accumulatorAge > nextAge ? accumulator : next;
+    }, 0)
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
