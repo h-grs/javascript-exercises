d46fa74c25400ecb1e58bf9412fd442b4793e6e5
hgrs <hugo.groschaus@protonmail.com>
2026-03-03 09:23:11 +0100

    Completed ex 14 - Fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    let count = 0;
 
+    if (typeof n !== 'number') {
+        count = parseInt(n)
+    } else { count = n }
+    if (count < 0) { return "OOPS"; } else if (count === 0) { return 0; }
+
+    let sum = 0
+    let num1 = 0;
+    let num2 = 1;;
+
+    for (let i = 3; i <= count; i++) {
+        sum = num1 + num2;
+        num1 = num2;
+        num2 = sum;
+    }
+    return num2;
 };
 
 // Do not edit below this line
